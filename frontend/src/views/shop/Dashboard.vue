012f66f71ef40ca23f295c1a5d23996b62e35ed2
Sreydeth1819 <sreydeth.khon@student.passerellesnumeriques.org>
2026-03-03 08:21:40 +0700

    improve lyout setting

--- frontend/src/views/shop/Dashboard.vue
+++ frontend/src/views/shop/Dashboard.vue
@@ -1,6 +1,6 @@
 <template>
   <main class="dashboard-page">
-    <h1>Shop Dashboard</h1>
+    <!-- <h1>Shop Dashboard</h1> -->
     <!-- <p>Dashboard page is ready for implementation.</p> -->
   </main>
 </template>
